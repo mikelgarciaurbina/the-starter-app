d538ddd74807b9f2806f4258f1b52c6358f5b52c
Mikel Garcia <mikel_6@hotmail.com>
2018-10-02 16:14:19 +0200

    Add FormTextInputs in the LoginScreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 
+import Button from "../components/Button";
+import FormTextInput from "../components/FormTextInput";
 import { STRINGS, THEME } from "../constants";
 import imageLogo from "../assets/images/logo.png";
 
 class LoginScreen extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Image source={imageLogo} style={styles.logo} />
-        <Text>{STRINGS.WELCOME_TO_LOGIN}</Text>
+        <View style={styles.form}>
+          <FormTextInput
+            value={this.state.email}
+            placeholder={STRINGS.EMAIL_PLACEHOLDER}
+          />
+          <FormTextInput
+            value={this.state.password}
+            placeholder={STRINGS.PASSWORD_PLACEHOLDER}
+          />
+        </View>
       </View>
     );
   }
@@ -21,6 +37,11 @@ const styles = StyleSheet.create({
     backgroundColor: THEME.WHITE,
     alignItems: "center",
     justifyContent: "space-between"
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    width: "80%"
   },
   logo: {
     flex: 1,
