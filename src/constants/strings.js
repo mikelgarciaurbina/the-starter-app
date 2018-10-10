2efc77532aa032156808dfec8605585b54a3406c
Mikel Garcia <mikel_6@hotmail.com>
2018-10-10 11:14:04 +0200

    Add error texts in strings.js

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,6 +1,8 @@
 const strings = {
+  EMAIL_REQUIRED: "Email is required",
   EMAIL_PLACEHOLDER: "Email",
   LOGIN: "Log In",
+  PASSWORD_REQUIRED: "Password is required",
   PASSWORD_PLACEHOLDER: "Password"
 };
 
